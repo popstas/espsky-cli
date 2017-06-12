a63f89eab93d9b4e3ca7fb5b6444b28f4dd0b075
Stanislav Popov <popstas@gmail.com>
2017-06-12 20:53:07 +0500

    feat: download file (to esp), without feedback

--- espsky.py
+++ espsky.py
@@ -7,13 +7,16 @@ from Crypto.Cipher import AES
 import json
 import hashlib
 import os
+import requests
 import StringIO
 import sys
 import paho.mqtt.client as mqtt
+import time
 
 parser = argparse.ArgumentParser(description='espsky-cli')
 parser.add_argument('command', help='Command')
 parser.add_argument('file_path', help='File path to upload', nargs='?', default=False)
+parser.add_argument('-n', '--filename', help='Filename, default from file url', nargs='?')
 parser.add_argument('-t', '--token', help='ESPSky device token', required=True)
 
 
@@ -30,6 +33,10 @@ def device_token_hash():
     return token_hash
 
 
+def content_signature(content):
+    return base64.b64encode(hashlib.sha512(content).digest())
+
+
 def mqtt_connect():
     client = mqtt.Client()
     if args.mqtt_user and args.mqtt_password:
@@ -38,17 +45,33 @@ def mqtt_connect():
     return client
 
 
-def mqtt_command(mqttc, command, command_args, body=''):
-    command_args['command'] = command
-    # print command_args
-    json_raw = json.dumps(command_args)
-    json_raw_aligned = _pad(json_raw)
+def encode_message(content):
+    aligned = _pad(content)
     key = args.token[0:16]
     iv = 16 * '\x00'
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    json_enc = cipher.encrypt(json_raw_aligned)
-    mqttc.publish('/%s/system/command' % device_token_hash(), bytearray(json_enc))
+    return cipher.encrypt(aligned)
+
+
+def mqtt_command(mqttc, command, command_args, body=''):
+    json_obj = {'command': command, 'args': command_args}
+    json_raw = json.dumps(json_obj)
+    mqttc.publish('/%s/system/command' % device_token_hash(), bytearray(encode_message(json_raw)))
     return False
+
+
+def download(file_url):
+    r = requests.get(file_url)
+    file_content = r.text
+    file_signature = content_signature(file_content)
+    filename = os.path.basename(file_url)
+    if args.filename:
+        filename = args.filename
+
+    mqttc = mqtt_connect()
+    command_args = {'name': filename, 'url': file_url, 'signature': file_signature}
+
+    mqtt_command(mqttc, 'system/file/download', command_args, file_content)
 
 
 def restart():
@@ -71,6 +94,8 @@ def main():
         print('host not defined')
         sys.exit(1)
 
+    elif args.command == 'download':
+        download(args.file_path)
     elif args.command == 'restart':
         restart()
     else:
